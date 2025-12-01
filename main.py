37071b3a7d2c7c24d862d246efff1eb321d4119e
Fardin Ahmed Azim <fardinazim7@gmail.com.com>
2025-12-01 22:51:10 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import face_recognition
+from playsound3 import playsound
+import threading
+cap = cv2.VideoCapture(0)
+sound_played = False
+
+def ring_bell():
+    playsound('doorbell.wav')
+
+
+while True:
+    _ , frame = cap.read()
+    faces = face_recognition.face_location(frame)
+    if faces and not sound_played:
+        threading.Thread(target=ring_bell).start()
+        sound_played=True
+    elif not faces and sound_played:
+        sound_played=False
+    cv2.imshow('Smart Doorbell', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
